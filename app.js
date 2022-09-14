356de27d9b794c1fd071e7da7384ef1983f7f120
Tyler Watson <tylerww91@gmail.com>
2022-09-14 12:58:46 -0700

    linked middle and pants portions of displayCharacter as well as their respective addEventListeners for for character design and display functionality

--- app.js
+++ app.js
@@ -25,18 +25,19 @@ headSelect.addEventListener('change', () => {
 });
 
 middleSelect.addEventListener('change', () => {
-    // > set the character middle from the value of the middle <select>
-    // > redisplay the character
+    character.middle = middleSelect.value;
+    displayCharacter();
 });
 
 pantsSelect.addEventListener('change', () => {
-    // > set the character head from the value of the head <select>
-    // > redisplay the character
+    character.pants = pantsSelect.value;
+    displayCharacter();
 });
 
 function displayCharacter() {
     headImage.src = 'assets/character/' + character.head + '-head.png';
-    // > set the middle and pants images .src property based on the character middle and pants properties
+    middleImage.src = 'assets/character/' + character.middle + '-middle.png';
+    pantsImage.src = 'assets/character/' + character.pants + '-pants.png';
 }
 
 function displayPhrases() {
